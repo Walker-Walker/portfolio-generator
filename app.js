d3b4f848885ed05eb771a02f15c1acba624128c9
Walker-Walker <awalker0@gmail.com>
2020-07-15 21:22:00 -0700

    added inquirer, and gitignore for node modules

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+const inquirer = require('inquirer');
+
+
+inquirer
+.prompt([
+  {
+  type: 'input',
+  name: 'name',
+  message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
+
+
+
+
 // start Capture input
-const fs = require("fs");
+// const fs = require("fs");
 
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
+// fs.writeFile("index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
   
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
 // const printProfileData = (profileDataArr) => {
 //     for (let i = 0; i < profileDataArr.length; i += 1) {
